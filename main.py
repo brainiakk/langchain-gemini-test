675e94adfad3f9d3584e94a831284ad41fffb071
Austin Brain <cybrainfoxy@gmail.com>
2024-05-25 15:42:57 +0100

    Initial commit 🙃

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_vertexai import ChatVertexAI
+
+load_dotenv()
+
+llm = ChatVertexAI(model_name="gemini-pro", temperature=0.4)
+
+while True:
+    text = input("Enter your query here >>>> ")
+    response = llm.invoke(text)
+    print(response.content)
